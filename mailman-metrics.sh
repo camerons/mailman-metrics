068b0f6b78d822106756c0720b8406842ced460c
camerons <cameron.shorter@gmail.com>
2016-11-07 06:21:23 +1100

    Extend mailman-metrics to count number of people posting

--- mailman-metrics.sh
+++ mailman-metrics.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This scrapes OSGeo mailman email list archives to count number of emails/month
+# This scrapes OSGeo mailman email list archives to count emails/month and emails/person
 
 # Copyright (c) 2016 Cameron Shorter
 # Licenced under the GNU LGPL version 3.
@@ -20,10 +20,22 @@
 listOfLists=https://lists.osgeo.org/mailman/listinfo
 baseDir=https://lists.osgeo.org/pipermail # Base directory of email archive
 #list=discuss #email list
-out="listposts.csv" #outfile
 
+# Output files
+listPostsOut="listposts.csv" # outfile counting number of posts per month for each list
+emailsPerPersonOut="emailsPerPerson.csv" # outfile counting number of emails per person
+emailsPerPersonPerYearOut="emailsPerPerson.csv" # outfile counting number of emails per person
+listsOut="listsOut.csv" # list of email lists being trawled
 
-echo "Date, List, Num Posts" > ${out}
+declare -A posters # hash array, number of email posts per person
+declare -A postersYear # hash array, number of email posts per person per year
+declare -A archive # hash array, number of posts per list per month
+
+# Write header line for output files
+echo "Year-Month-List, Num Posts" > ${listPostsOut}
+echo "Emailer, Total Emails" > $emailsPerPersonOut
+echo "Year-Emailer, EmailsPerYear" > $emailsPerPersonPerYearOut
+echo "List" > ${listsOut}
 
 # Get a list of OSGeo email lists
 for list in \
@@ -38,10 +50,40 @@ do
     | sed -e's#^.*href="##' \
     | sed -e's#".*$##'`
   do
-    echo -n "${dir}" | sed -e's#/.*$##' >> $out # Print Year-Month
-    echo -n ",${list}" >> $out
-    echo -n ", "${baseDir}/${list}/${dir}", " >> $out
+    echo dir="${dir}"
+    year=`echo "${dir}" | sed -e's#-.*$##'`
+    month=`echo "${dir}" | sed -e's#^.*-##' | sed -e's#/.*$##'`
+    echo "${list}" >> $listsOut
+
     # search month page and count number of posts
-    wget -O - ${baseDir}/${list}/${dir} | grep "<LI><A" | wc -l >> $out
+    #wget -O - ${baseDir}/${list}/${dir} | grep "<LI><A" | wc -l >> $listPostsOut
+
+    # Extract list of people sending emails this month
+    IFS=$'\n'
+    set -f
+    for name in `wget -O - ${baseDir}/${list}/${dir} | grep "^<I>" | sed -e's#<I>##'` 
+    do
+      postersYear["$year-$name"]=$((postersYear["$year-$name"]+1))
+      posters["$name"]=$((posters["$name"]+1))
+      archive["$year-$month-$list"]=$((archive["$year-$month-$list"]+1))
+    done
+
+    # print out number of posts per list, per month
+    for name in "${!archive[@]}"
+    do
+      echo $name,${archive[$name]} >> $listPostsOut
+    done
+
+    # print out number of posts per poster
+    for name in "${!postersYear[@]}"
+    do
+      echo $name,${postersYear[$name]} >> $emailsPerPersonPerYearOut
+    done
+
+    # print out number of posts per poster, per year
+    for name in "${!posters[@]}"
+    do
+      echo $name,${posters[$name]} >> $emailsPerPersonOut
+    done
   done
 done
